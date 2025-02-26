719d10f71c608362b9d0147f2014a208a3ed88ad
Jae-KimSeo <seojaeinblank@gmail.com>
2025-02-26 17:23:36 +0900

    Initial commit

---
+++ src/main/java/org/cody/codyservice/CodyServiceApplication.java
@@ -0,0 +1,13 @@
+package org.cody.codyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodyServiceApplication.class, args);
+	}
+
+}
