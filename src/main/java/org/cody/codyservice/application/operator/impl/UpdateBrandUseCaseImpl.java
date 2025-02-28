7afe8fce5044a210e800302fbedf5b8c4464d8a6
Jae-KimSeo <seojaeinblank@gmail.com>
2025-02-28 15:11:04 +0900

    Add server.log to .gitignore

--- src/main/java/org/cody/codyservice/application/operator/impl/UpdateBrandUseCaseImpl.java
+++ src/main/java/org/cody/codyservice/application/operator/impl/UpdateBrandUseCaseImpl.java
@@ -19,16 +19,13 @@ public class UpdateBrandUseCaseImpl implements UpdateBrandUseCase {
     
     @Override
     public Brand updateBrand(Integer id, Brand updatedBrand) {
-        // Optional 대신 직접 null 체크
         Brand existingBrand = brandRepository.findById(id);
         if (existingBrand == null) {
             throw new BusinessException("브랜드를 찾을 수 없습니다: " + id);
         }
         
-        // 업데이트할 필드 설정
         existingBrand.setName(updatedBrand.getName());
         
-        // 수정 시간 업데이트
         existingBrand.setUpdatedAt(LocalDateTime.now());
         
         return brandRepository.save(existingBrand);
